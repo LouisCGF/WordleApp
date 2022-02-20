e0b80500efd7c63e05024ab142e6bd30ce67b5ae
louis <c0080358@newcastle.ac.co.uk>
2022-02-20 22:00:59 +0000

    Created methods displayLetter, removeLetter and generalKeyClick to display and remove letters on the grid Set up click listeners for keyboard buttons

--- app/src/main/java/com/example/wordleapp/MainActivity.java
+++ app/src/main/java/com/example/wordleapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -20,13 +21,15 @@ public class MainActivity extends AppCompatActivity {
             viewGrid1_2, viewGrid1_3, viewGrid1_4, viewGrid2_0, viewGrid2_1, viewGrid2_2, viewGrid2_3,
             viewGrid2_4, viewGrid3_0, viewGrid3_1, viewGrid3_2, viewGrid3_3, viewGrid3_4, viewGrid4_0,
             viewGrid4_1, viewGrid4_2, viewGrid4_3, viewGrid4_4, viewGrid5_0, viewGrid5_1, viewGrid5_2,
-            viewGrid5_3, viewGrid5_4;
+            viewGrid5_3, viewGrid5_4, currentView;
 
     private TextView textGrid0_0, textGrid0_1, textGrid0_2, textGrid0_3, textGrid0_4, textGrid1_0, textGrid1_1,
             textGrid1_2, textGrid1_3, textGrid1_4, textGrid2_0, textGrid2_1, textGrid2_2, textGrid2_3,
             textGrid2_4, textGrid3_0, textGrid3_1, textGrid3_2, textGrid3_3, textGrid3_4, textGrid4_0,
             textGrid4_1, textGrid4_2, textGrid4_3, textGrid4_4, textGrid5_0, textGrid5_1, textGrid5_2,
-            textGrid5_3, textGrid5_4;
+            textGrid5_3, textGrid5_4, currentTextView;
+
+    boolean isRowFull = false;
 
     View[][] views;
     TextView[][] textViews;
@@ -92,10 +95,89 @@ public class MainActivity extends AppCompatActivity {
         textGrid5_1 = findViewById(R.id.textGrid5_1); textGrid5_2 = findViewById(R.id.textGrid5_2);
         textGrid5_3 = findViewById(R.id.textGrid5_3); textGrid5_4 = findViewById(R.id.textGrid5_4);
 
+
+// ------------------------- BUTTONS LISTENERS ------------------------------------------------------
+
+        deleteButton.setOnClickListener(view -> {
+            isRowFull = false;
+            if (currentColumn != 0){
+                currentColumn--;
+            }
+            removeLetter(currentColumn, currentRow);
+        });
+
+        enterButton.setOnClickListener(view -> {
+            if (currentColumn != 5){
+                Toast.makeText(MainActivity.this, "Please enter a five letter word", Toast.LENGTH_SHORT).show();
+            } else{
+                // will do something like compareInputWord()
+                currentRow++;
+                currentColumn = 0;
+                isRowFull = false;
+            }
+        });
+
+        qButton.setOnClickListener(view -> generalKeyClick("Q"));
+        wButton.setOnClickListener(view -> generalKeyClick("W"));
+        eButton.setOnClickListener(view -> generalKeyClick("E"));
+        rButton.setOnClickListener(view -> generalKeyClick("R"));
+        tButton.setOnClickListener(view -> generalKeyClick("T"));
+        yButton.setOnClickListener(view -> generalKeyClick("Y"));
+        uButton.setOnClickListener(view -> generalKeyClick("U"));
+        iButton.setOnClickListener(view -> generalKeyClick("I"));
+        oButton.setOnClickListener(view -> generalKeyClick("O"));
+        pButton.setOnClickListener(view -> generalKeyClick("P"));
+        aButton.setOnClickListener(view -> generalKeyClick("A"));
+        sButton.setOnClickListener(view -> generalKeyClick("S"));
+        dButton.setOnClickListener(view -> generalKeyClick("D"));
+        fButton.setOnClickListener(view -> generalKeyClick("F"));
+        gButton.setOnClickListener(view -> generalKeyClick("G"));
+        hButton.setOnClickListener(view -> generalKeyClick("H"));
+        jButton.setOnClickListener(view -> generalKeyClick("J"));
+        kButton.setOnClickListener(view -> generalKeyClick("K"));
+        lButton.setOnClickListener(view -> generalKeyClick("L"));
+        zButton.setOnClickListener(view -> generalKeyClick("Z"));
+        xButton.setOnClickListener(view -> generalKeyClick("X"));
+        cButton.setOnClickListener(view -> generalKeyClick("C"));
+        vButton.setOnClickListener(view -> generalKeyClick("V"));
+        bButton.setOnClickListener(view -> generalKeyClick("B"));
+        nButton.setOnClickListener(view -> generalKeyClick("N"));
+        mButton.setOnClickListener(view -> generalKeyClick("M"));
+
     }
 
-    private void displayLetter(int currentColumn, int currentRow){
-        String currentGridViewID = "viewGrid" + currentRow + "_" + currentRow;
+    private void generalKeyClick(String letter){
+        if (!isRowFull){
+            if (currentColumn != 5){
+                displayLetter(currentColumn, currentRow, letter);
+                currentColumn++;
+            } else{
+                isRowFull = true;
+            }
+        }
+    }
+
+    private void displayLetter(int currentColumn, int currentRow, String letter){
+        String currentViewID = "viewGrid" + currentRow + "_" + currentColumn;
+        String currentTextViewID = "textGrid" + currentRow + "_" + currentColumn;
+
+        currentView = findViewById(getResources().getIdentifier(currentViewID, "id", getPackageName()));
+        currentTextView = findViewById(getResources().getIdentifier(currentTextViewID, "id", getPackageName()));
+
+        currentView.setAlpha((float) 1);
+        currentTextView.setText(letter);
+
+    }
+
+    private void removeLetter(int currentColumn, int currentRow){
+        String currentViewID = "viewGrid" + currentRow + "_" + currentColumn;
+        String currentTextViewID = "textGrid" + currentRow + "_" + currentColumn;
+
+        currentView = findViewById(getResources().getIdentifier(currentViewID, "id", getPackageName()));
+        currentTextView = findViewById(getResources().getIdentifier(currentTextViewID, "id", getPackageName()));
+
+        currentView.setAlpha((float) 0.2);
+        currentTextView.setText("");
 
     }
 
